95801b13188797df7a1beb86c73c16b7fdf280a5
cristiancmach <cristianc1099@gmail.com>
2024-05-30 13:52:56 -0500

    Neavbar Fixed 2nd Deploy

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,13 @@
-'use client'
-import React from 'react'
-import { AudioLines, ChevronLast, ChevronFirst, Home } from "lucide-react";
+'use client';
+import React from 'react';
+import { AudioLines, ChevronLast, ChevronFirst } from "lucide-react";
 
 interface NavbarProps {
   urlNext: string;
   urlBefore: string;
 }
 
-const Navbar = ({urlNext, urlBefore}) => {
+const Navbar: React.FC<NavbarProps> = ({ urlNext, urlBefore }) => {
   return (
     <div className="fixed z-40 text-white flex items-center justify-center w-full mt-auto h-max bottom-12">
         <div className="gap-6 px-4 grid grid-cols-3 py-1 rounded-full bg-white/15 background-blur-sm">
@@ -16,7 +16,7 @@ const Navbar = ({urlNext, urlBefore}) => {
             <a href={urlNext} className='px-1'> <ChevronLast /> </a>
         </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
