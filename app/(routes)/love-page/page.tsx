56c9d21a06143f7d6b3ec3becca86a34c59432b2
cristiancmach <cristianc1099@gmail.com>
2024-08-04 17:50:00 -0500

    AudioUrl en cada modulo

--- app/(routes)/love-page/page.tsx
+++ app/(routes)/love-page/page.tsx
@@ -8,7 +8,7 @@ const Love = () => {
     return (
         <div className="justify-center content-center">
             <ImageBg src='/img/love.jpg' alt="Imagen de Amor" /> 
-            <Navbar urlBefore='/energia-page' urlNext='/paz-page' />
+            <Navbar urlBefore='/energia-page' urlNext='/paz-page' audioSrc="/music/paz.mp3" />
             <ButtonHome />
             <TitleExp>Love Experience</TitleExp>
         </div>
